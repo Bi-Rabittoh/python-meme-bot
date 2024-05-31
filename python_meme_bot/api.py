9ab1c46da6bfcb2c0526266cea0e9f267ef0c40f
BiRabittoh <andronacomarco@gmail.com>
2024-05-31 03:07:18 +0200

    remove useless library, bump python-telegram-bot version

--- python_meme_bot/api.py
+++ python_meme_bot/api.py
@@ -3,13 +3,19 @@ import requests
 from io import BytesIO
 
 from PIL import Image, UnidentifiedImageError
-from anime_api.apis import WaifuPicsAPI
-from anime_api.apis.waifu_pics.types import ImageCategory
 
 max_tries = 5
 supported_file_types = [".jpg", ".jpeg", ".png"]
-api = WaifuPicsAPI()
+endpoint = "https://api.waifu.pics"
 
+class APIException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Error {self.status_code}: {self.message}"
 
 def _valid_extension(fname: str):
     for t in supported_file_types:
@@ -17,25 +23,30 @@ def _valid_extension(fname: str):
             return True
     return False
 
-
 def get_random_image(nsfw=False):
-    cat = ImageCategory.NSFW.WAIFU if nsfw else ImageCategory.SFW.WAIFU
-
     count = 0
     while count < max_tries:
         try:
-            img = api.get_random_image(category=cat)
-            if not _valid_extension(img.url):
-                raise Exception
-            r = requests.get(img.url)
-            image = Image.open(BytesIO(r.content))
+            apiUrl = endpoint + f"/{'nsfw' if nsfw else 'sfw'}/waifu"
+            response = requests.get(apiUrl)    
 
-            return image, img.url
+            content = response.json()
+            if response.status_code != 200:
+                raise APIException(response.status_code, content['message'])
+            
+            url = content["url"]
+            if not _valid_extension(url):
+                raise AssertionError
+            
+            r = requests.get(url)
+            b = BytesIO(r.content)
+            image = Image.open(b)
+            return image, url
 
-        except (KeyError, IndexError, Exception):
+        except (KeyError, IndexError, AssertionError, APIException):
             logging.warning("Can't display image.")
         except UnidentifiedImageError:
-            logging.warning("Unidentified image: " + img.url)
+            logging.warning("Unidentified image: " + url)
 
         count += 1
         logging.warning(f"Try #{count} failed.\n")
